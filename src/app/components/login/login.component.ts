ac5b8696137f98a0dc881a36c14a8ba4a02be9d4
balaramgayen <balaramgayen53501@gmail.com>
2022-04-01 19:01:17 +0530

    deploy on firebase done and continue routing

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  constructor(private afAuth: AngularFireAuth) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  login() {
+    alert('login works');
   }
-
 }
